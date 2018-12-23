f5b212efd49b5206c216de8d8ca27010a2e1b150
brian-quach <briiquach@gmail.com>
2018-12-23 14:20:54 -0500

    Camera settings

---
+++ Jetroid/Assets/Jetroid/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+    public float scale = 4f;
+
+    private Transform t;
+
+    private void Awake() {
+        var cam = GetComponent<Camera>();
+        cam.orthographicSize = (Screen.height / 2f) / scale;
+    }
+
+    void Start () {
+        t = target.transform;
+	}
+	
+	void Update () {
+		if(target != null) {
+            transform.position = new Vector3(t.position.x, t.position.y, transform.position.z);
+        }
+	}
+}
